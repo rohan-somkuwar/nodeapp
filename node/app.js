d965b4bcb1fb92a8bbe6f367d8389fc2c6200dbc
RohanS <rohansomkuwar97@gmail.com>
2022-08-11 18:22:24 +0530

    deploy infra

---
+++ node/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res)=> {
+    res.send("Service is up and running");
+});
+app.listen(8080,()=>{
+    console.log("server is up");
+});
